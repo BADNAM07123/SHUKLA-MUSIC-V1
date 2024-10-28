abd0210aaaa8b40ab79e324211ffe8232d5cb5f0
BADNAM07123 <kb7266411@gmail.com>
2024-10-28 16:23:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/BADNAM07123/SHUKLA-MUSIC-V1",
+    "https://github.com/BADNAM07123/SHUKLA-MUSIC-V1.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
